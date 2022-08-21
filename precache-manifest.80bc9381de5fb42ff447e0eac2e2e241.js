60164c728ce7a61bd8eb3d2113d8148e2952bf9c
Munaf <muhammadmunaf>
2022-08-21 19:54:17 +0500

    Updates

---
+++ precache-manifest.80bc9381de5fb42ff447e0eac2e2e241.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "99035db7654fc906fca4",
+    "url": "/static/css/main.544ee536.chunk.css"
+  },
+  {
+    "revision": "99035db7654fc906fca4",
+    "url": "/static/js/main.99035db7.chunk.js"
+  },
+  {
+    "revision": "317263d0013363540c0f",
+    "url": "/static/js/1.317263d0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4c6034b7aa0c0810c79bad065f06b06b",
+    "url": "/static/media/linkedin.4c6034b7.svg"
+  },
+  {
+    "revision": "8727bf01a16540047cde381db0541241",
+    "url": "/static/media/instagram.8727bf01.svg"
+  },
+  {
+    "revision": "ee56df9a69a9dd9be3379cf77b4bccf9",
+    "url": "/static/media/twitter.ee56df9a.svg"
+  },
+  {
+    "revision": "f3547a4aa7c46afb9600542a2ce87274",
+    "url": "/static/media/medium.f3547a4a.svg"
+  },
+  {
+    "revision": "d4d6ec239d1463313a2bd4242df6c673",
+    "url": "/static/media/arrow_right.d4d6ec23.svg"
+  },
+  {
+    "revision": "9d4a7badefbe672ca7929b063cb5e568",
+    "url": "/index.html"
+  }
+];
